fee103d1d0b3d19c75679181af1e71d5a148a19a
Raj Paswan <131708854+Raj-git23@users.noreply.github.com>
2025-03-21 05:29:43 +0530

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -93,7 +93,7 @@ export default function Dashboard() {
   const [routeData, setRouteData] = useState<any>(null);
   const [map, setMap] = useState<any>(null);
   const [marker, setMarker] = useState<any>(null);
-  const mapplsApiKey = "35dd838f-d399-4733-84af-b48b430f25fa"; // Replace with your API key
+  const mapplsApiKey = "38dec545-b224-471d-97bd-674a52a3b09c"; // Replace with your API key
   interface NominatimResponse {
     display_name: string;
     error?: string;
